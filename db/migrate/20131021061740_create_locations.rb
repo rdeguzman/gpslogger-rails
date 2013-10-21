50e38b7d45e7b33d150625acd5291647ac026a29
Rupert <rupert@datalinktech.com.au>
2013-10-21 17:32:22 +1100

    Adds "Location" model

---
+++ db/migrate/20131021061740_create_locations.rb
@@ -0,0 +1,13 @@
+class CreateLocations < ActiveRecord::Migration
+  def change
+    create_table :locations do |t|
+      t.integer :gps_timestamp, :limit => 8, :default => 0, :null => false
+      t.float :gps_latitude, :default => 0.0, :null => false
+      t.float :gps_longitude, :default => 0.0, :null => false
+      t.float :gps_speed
+      t.float :gps_heading
+
+      t.timestamps
+    end
+  end
+end
