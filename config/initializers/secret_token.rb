e717d2d02dd064294752d87822f7812da72b981f
Rupert <rupert@datalinktech.com.au>
2013-10-21 13:33:51 +1100

    Initial rails4 project structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gpslogger::Application.config.secret_key_base = 'b1cd7ab0f5af0f753a2ab9c23acb51e3ecaa066f4502930966e2a51f5ed14fd6d49accb3fecb0a905ad362ba6711143b405ff962a24368885fc0b91674bb52e4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gpslogger::Application.config.session_store :cookie_store, key: '_gpslogger_session'
