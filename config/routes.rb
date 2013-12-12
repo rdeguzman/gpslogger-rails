28b1888f89ef1101606d83c3a14b00555b25155f
rupert <rupert@2rmobile.com>
2013-12-13 10:09:17 +1100

    Add devise_for :users in routes.rb  Change root_path to point to ‘home/index’

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Gpslogger::Application.routes.draw do
+  devise_for :users
+
   resources :locations
+
   get "map" => "mapper#public"
   get "map/public" => "mapper#public"
-  root :to => 'mapper#public'
+
+  get "home/index"
+
+  root :to => 'home#index'
 end
